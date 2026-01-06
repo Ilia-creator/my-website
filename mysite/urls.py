910df1a48ca2db8e227af6d9fb7fec9fe7adef05
Alexey Shubnikov <shubnikov.av@gmail.com>
2026-01-06 23:01:08 +0200

    Add middleware to route admin page

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,6 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('src.urls')),
-    path('admin/', admin.site.urls),
     path('game/', include('game.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
